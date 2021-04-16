4bf9eb80e8fbebd4072388080040eafd8538ffa8
limjunsung225 <55376155+limjunsung225@users.noreply.github.com>
2021-04-16 14:46:23 +0900

    ljs ( item ui and npc )

---
+++ Assets/Scripts/ItemSystem/MPItemUse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MPItemUse : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.inputString == (transform.parent.GetComponent<Slot>().num + 1).ToString()) 
+        {
+            // 아이템 사용
+            Debug.Log(" MP 상승 , slotNumber : " + (transform.parent.GetComponent<Slot>().num + 1));
+            Destroy(this.gameObject);
+        }
+    }
+}
