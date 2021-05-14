7a749c2558518443f49de3ec3742b3f362818a15
727207e <54798151+727207e@users.noreply.github.com>
2021-05-14 15:04:30 +0900

    명성님
    5월14일

---
+++ Assets/Scenes/achScene/gateOpenControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gateOpenControl : MonoBehaviour
+{
+    public GameObject open_Gate;
+    private GameObject[] gates;
+    private GameObject map;
+    private int map_Position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gates = mapscript.instance.gates;
+        map_Position = mapscript.instance.stage_Position;
+        map = mapscript.instance.map_List[map_Position];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mapscript.instance == null)
+            print("dd");
+
+        // 몬스터가 없으면
+        if (mapscript.instance.monster_count == 0)
+        {
+            // 왼쪽 게이트일 경우 배열 0번에 오른쪽 게이트일 경우 배열 1번에
+            if (gameObject.name == "LGate")
+            {
+                gates[0] = Instantiate(open_Gate, map.transform) as GameObject;
+                gates[0].transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 5, 0);
+            }
+            if (gameObject.name == "RGate")
+            {
+                gates[1] = Instantiate(open_Gate, map.transform) as GameObject;
+                gates[1].transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 5, 0);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
