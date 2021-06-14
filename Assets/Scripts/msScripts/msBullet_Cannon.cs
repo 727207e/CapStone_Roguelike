de095ca7b757f9db5b98446c1832711835d35afe
Codocto <ilsuntwo@naver.com>
2021-06-14 15:16:08 +0900

    ms210614
    .

---
+++ Assets/Scripts/msScripts/msBullet_Cannon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class msBullet_Cannon : MonoBehaviour
+{
+    public float bulletSpeed = 1000.0f;
+    public int bulletDamage = 0;
+    public GameObject splashEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FireBullet()
+    {
+        Debug.Log(gameObject.transform);
+        GetComponent<Rigidbody>().AddForce(gameObject.transform.forward * bulletSpeed);
+        Debug.Log("Bullet Fired with" + bulletDamage + " Damage");
+    }
+
+    public void SetBulletDamage(int x)
+    {
+        bulletDamage = x;
+        Debug.Log("Set Bullet Damage " + bulletDamage);
+    }
+
+    public void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.layer == LayerMask.NameToLayer("Ground") || coll.gameObject.tag == "Monster")
+        {
+            Debug.Log("Trigger Enter");
+            Vector3 pos = gameObject.transform.position;
+            Instantiate(splashEffect, pos, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/msScripts/msCannon.cs
+++ Assets/Scripts/msScripts/msCannon.cs
@@ -6,10 +6,20 @@ public class msCannon : MonoBehaviour
 {
     public GameObject bulletPrefab; //사용하는 총알 이후 총이랑 따로 분리해야함.
     public Transform muzzleTransform; //총알이 발사되는 곳
-    public int fullAmmo = 10;
+    public ParticleSystem muzzleFlash;
+
+    public int fullAmmo = 5;
     public int currentAmmo;
-    public float reloadTime = 3.0f; //재장전 시간
+
+    public float reloadTime = 6.0f; //재장전 시간
     public bool onReload; //현재 재장전 중인가?
+
+    public int cannonDamage = 60; //총 데미지
+    public float fireRate = 2.00f; //연사속도
+    public bool isFiring = false;
+    public float timer;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,14 +31,57 @@ public class msCannon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Fire();
-        }
         if (Input.GetKeyDown(KeyCode.R))
         {
             StartCoroutine(Reload());
         }
+
+        if (Input.GetButton("Fire1"))
+        {
+            isFiring = true;
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            isFiring = false;
+        }
+
+        if (timer >= fireRate)
+        {
+            if (isFiring == true)
+            {
+                if (onReload == false)
+                {
+                    if (currentAmmo == 0)
+                    {
+                        Debug.Log("재장전이 필요합니다.");
+                    }
+                    else
+                    {
+                        var go = Instantiate(bulletPrefab);
+                        go.transform.position = muzzleTransform.position;
+                        go.transform.rotation = muzzleTransform.rotation;
+                        var bullet = go.GetComponent<msBullet_Cannon>();
+                        bullet.SetBulletDamage(cannonDamage);
+                        bullet.FireBullet();
+                        //Instantiate(bulletPrefab, muzzleTransform.position, muzzleTransform.rotation);
+                        //muzzleFlash.Emit(1);
+                        currentAmmo--;
+                        Debug.Log("남은 장탄량 : " + currentAmmo);
+                    }
+                }
+                else
+                {
+                    Debug.Log("재장전 중입니다.");
+                }
+            }
+            timer = 0.0f;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+
     }
 
     private void Fire()
@@ -59,17 +112,12 @@ public class msCannon : MonoBehaviour
 
     public IEnumerator Reload()
     {
-        int countTime = 0;
         onReload = true;
         Debug.Log("재장전중...");
 
-        while (countTime < 10)
-        {
-            Debug.Log("재장전중..." + countTime);
-            yield return new WaitForSeconds(0.2f);
 
-            countTime++;
-        }
+        yield return new WaitForSeconds(reloadTime);
+
         currentAmmo = fullAmmo;
         Debug.Log("재장전 완료");
         onReload = false;
