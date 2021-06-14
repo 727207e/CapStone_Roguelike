41711a7764efc65592ea7e8bc6b139e2b5bfed91
Hwa-y <dcg0227@naver.com>
2021-06-14 15:04:19 +0900

    6/14
    Monster+Placing+AttackAdd

---
+++ Assets/Scripts/achScripts/monster/HpBarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpBarController : MonoBehaviour
+{
+
+    private Transform cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(transform.position + cam.rotation * Vector3.forward, cam.rotation * Vector3.up);
+    }
+}
